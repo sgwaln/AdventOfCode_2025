d86bf932c64b430d8940442d1229c687e41edd66
sgwaln <sethgwaln@gmail.com>
2025-12-05 17:23:37 -0900

    Day 4 done

--- day3.py
+++ day3.py
@@ -1,34 +1,55 @@
 file_path = "day3_input.txt"
 
 total = 0
+number_of_batteries = 12
+batteries  = [0]*number_of_batteries
+
+def find_max(list):
+    index = 0
+    max = list[0]
+    for i in range(1,len(list)):
+        if list[i] > max:
+            max = list[i]
+            index = i
+    return max, index
+
 try:
     with open(file_path, 'r') as file:
 
         line_count = 0
         for line in file:
             line = line.strip()
+            bank = [int(char) for char in line]
+            bank_size = len(bank)
 
-            pressident = int(line[0])
-            vp = int(line[1])
+            batteries = bank[:number_of_batteries]
+            print(f"Bank: {line}")
 
-            for index in range(1,len(line)-1):
-                candidate = int(line[index])
+            start_range = 0
+            end_range = bank_size - number_of_batteries + 1
+            avalible_range = bank[start_range:end_range]
+            batteries[0], bank_index = find_max(avalible_range)
+            print(f"Start: {start_range}, End: {end_range}, Bank: {bank_index}")
+            print(f"Range = {avalible_range}, Battery: {batteries[0]}")
 
-                if candidate > pressident: # Tens is bigger, so number bigger, assume index after as vp
-                    pressident = candidate
-                    vp = int(line[index + 1])
-                elif candidate > vp: # While the pressident didn't get beat, the VP still can
-                    vp = candidate
 
-            # Didn't catch the edge case of last number in line being larger then vp
-            last = int(line[-1])
-            if last > vp:
-                vp = last
+            for battery_index in range(1,len(batteries)):
+                start_range = bank_index + 1 
+                end_range = bank_size - number_of_batteries + 1 + battery_index
+                avalible_range = bank[start_range:end_range]
+                batteries[battery_index], bank_index = find_max(avalible_range)
+                print(f"Start: {start_range}, End: {end_range}, Bank: {bank_index}")
+                print(f"Range = {avalible_range}, Battery: {batteries[battery_index]}")
+                bank_index = start_range + bank_index
 
-            total += pressident*10 + vp
-            line_count += 1
-            print(f"P:{pressident} VP:{vp}\t Total: {total} \t Line Number: {line_count-1}")
-        
+            print(f"Batteries: {batteries}")
+            jolt = 0
+            for battery_index in range(0,len(batteries)):
+                jolt += int(batteries[battery_index] * (10**(number_of_batteries - 1 - battery_index)))
+            print(f"Joltage Total: {jolt}")
+            total += jolt
+            print("\r\n"*2)
+
 except Exception as e:
     print(f"An error occurred: {e}")
 
--- day4.py
+++ day4.py
@@ -1,10 +1,57 @@
+import copy
+
 file_path = "day4_input.txt"
 
 total = 0
+max_neigbors = 4
+empty = 0
+
+def count_neigbors(grid,pos_x,pos_y):
+    y_max = len(grid)
+    x_max = len(grid[0])
+    count = 0
+
+    for x in range(-1,1+1):
+
+        x_cord = pos_x + x
+        if x_cord < 0 or x_cord >= x_max:
+            continue
+
+        for y in range(-1,1+1):
+            if x == 0 and y == 0: # Don't count yourself
+                continue
+            
+            y_cord = pos_y + y
+            if y_cord < 0 or y_cord >= y_max:
+                continue
+
+            if grid[y_cord][x_cord] != empty:
+                count += 1
+    return count
 
 try:
     with open(file_path, 'r') as file:
-        grid = file.read().replace('@','1').replace('.','0')
+        grid = list(file.read().replace('@','1').replace('.','0').split('\n'))
+        grid_new = [[int(digit) for digit in row] for row in grid]
+
+        grid_old = [[0 for _ in range(len(grid_new[0]))] for _ in range(len(grid_new))]
+
+        y_max = len(grid)
+        x_max = len(grid[0])
+
+        interations = 0
+
+        while grid_new != grid_old:
+            grid_old = copy.deepcopy(grid_new)  # Create a true copy            print('-'*50)
+            print(f"Interation: {interations}")            
+            for y in range(0,y_max):
+                for x in range(0,x_max):
+                    if grid_old[y][x] != empty:
+                        if count_neigbors(grid_old,x,y) < max_neigbors:
+                            total += 1
+                            grid_new[y][x] = 0
+            interations += 1
+
 
 except Exception as e:
     print(f"An error occurred: {e}")
